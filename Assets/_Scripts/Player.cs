f24be16e81592c6ed3c2d34fbbcf97feae3be0e1
dr-matt-smith <dr_matt_smith@me.com>
2017-08-21 13:10:27 +0100

    refactored PlayerInventory class as Player

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class PlayerInventory : MonoBehaviour
+public class Player : MonoBehaviour
 {
 	private InventoryManager inventoryManager;
 
--- Assets/_Scripts/PlayerInventoryDisplay.cs
+++ Assets/_Scripts/PlayerInventoryDisplay.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 using System.Collections.Generic;
 
-[RequireComponent(typeof(PlayerInventory))]
+[RequireComponent(typeof(Player))]
 [RequireComponent(typeof(InventoryManager))]
 public class PlayerInventoryDisplay : MonoBehaviour
 {

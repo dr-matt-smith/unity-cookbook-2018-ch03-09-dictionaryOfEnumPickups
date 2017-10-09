60a2c4e9a3203ee32c0ebcd4183c7ebd9ccf64a3
dr-matt-smith <dr_matt_smith@me.com>
2017-10-09 10:11:44 +0100

    updated scene

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
 	private InventoryManager inventoryManager;
 
--- Assets/_Scripts/PlayerInventoryDisplay.cs
+++ Assets/_Scripts/PlayerInventoryDisplay.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 using System.Collections.Generic;
 
-[RequireComponent(typeof(Player))]
+[RequireComponent(typeof(PlayerController))]
 [RequireComponent(typeof(InventoryManager))]
 public class PlayerInventoryDisplay : MonoBehaviour
 {
